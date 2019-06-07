c0297cc722fd295e7edee78ff8fcc8fd0d640168
Arumuga <aruraja55@gmail.com>
2019-06-07 15:16:49 +0800

    First code push

---
+++ project.py
@@ -0,0 +1,9 @@
+def calc():
+    total = 0
+    name = input("what is the first name: ")
+    age = input("what is your age: ")
+    amount = int(input("how much you have: "))
+
+    print(name + " is " + age + " years old " + " and have the amount of " + str(amount))
+
+calc()    
