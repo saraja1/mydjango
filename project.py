308ebfda6d57fc0d02ba76c99cc9616dbb829028
Arumuga <aruraja55@gmail.com>
2019-06-07 17:14:28 +0800

    Revert "hello"
    This reverts commit 7a976ead29f1c0e7eae231e940530174ea277e98.

--- project.py
+++ project.py
@@ -6,6 +6,5 @@ def calc():
     location = input("Where he located: ")
 
     print(name + " is " + age + " years old " + " and have the amount of " + str(amount) + "." + " He is located on " + location)
-    print("Hello World")
 
 calc()    
