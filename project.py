b932f0b959ef2a3270673d5c0418cba2a7918592
Arumuga <aruraja55@gmail.com>
2019-06-07 16:39:44 +0800

    location added

--- project.py
+++ project.py
@@ -3,7 +3,8 @@ def calc():
     name = input("what is the first name: ")
     age = input("what is your age: ")
     amount = int(input("how much you have: "))
+    location = input("Where he located: ")
 
-    print(name + " is " + age + " years old " + " and have the amount of " + str(amount))
+    print(name + " is " + age + " years old " + " and have the amount of " + str(amount) + "." + " He is located on " + location)
 
 calc()    
